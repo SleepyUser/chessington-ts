b03bde957341cbd452a61b6f3f61abf63eb75ed7
Sleepyuser <36637730+SleepyUser@users.noreply.github.com>
2023-10-19 11:26:40 +0100

    Queen can move like the bishop and rook combined

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,42 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableSquares: Square[] =[];
+        const square = board.findPiece(this);
+        //Diagonal
+        for (let i = 1; Math.max(square.col +i, square.row + i) <= 7; i++)
+        {
+            availableSquares.push(Square.at( square.row + i,square.col + i));
+        }
+        for (let i = 1; Math.min( square.row  -i, square.col -i,) >= 0; i++)
+        {
+            availableSquares.push(Square.at(square.row - i, square.col - i));
+        }
+        for (let i = 1; square.row  + i <= 7 &&  square.col - i >= 0; i++)
+        {
+            availableSquares.push(Square.at(square.row + i, square.col - i));
+        }
+        for (let i = 1; square.row  - i >= 0 &&  square.col +i <= 7; i++)
+        {
+            availableSquares.push(Square.at(square.row - i, square.col + i));
+        }
+        //Straight lines
+        for (let i = square.row+1; i <= 7; i++)
+        {
+            availableSquares.push(Square.at(i, square.col));
+        }
+        for (let i = square.row-1; i >= 0; i--)
+        {
+            availableSquares.push(Square.at(i, square.col));
+        }
+        for (let i = square.col-1; i >= 0; i--)
+        {
+            availableSquares.push(Square.at(square.row, i));
+        }
+        for (let i = square.col+1; i <= 7; i++)
+        {
+            availableSquares.push(Square.at(square.row, i));
+        }
+        return availableSquares;
     }
 }
