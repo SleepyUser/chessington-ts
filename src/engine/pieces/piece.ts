4bcde5b22734425d85588e41ecc4cdf22e1595e5
Sleepyuser <36637730+SleepyUser@users.noreply.github.com>
2023-10-19 17:24:12 +0100

    Rooks can take any opposing piece except Kings.

--- src/engine/pieces/piece.ts
+++ src/engine/pieces/piece.ts
@@ -1,9 +1,12 @@
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import player from "../player";
+import King from "./king";
 
 export default class Piece {
     public player: Player;
+    public isKing: boolean = false;
 
     public constructor(player: Player) {
         this.player = player;
@@ -18,40 +21,48 @@ export default class Piece {
         board.movePiece(currentSquare, newSquare);
     }
 
-    protected isSpaceValid(board: Board, checkSquare: Square) {
-        return board.getPiece(checkSquare) === undefined;
-    }
-
 
     protected getOrthogonalMovement(square: Square, candidateSquare: Square | undefined, board: Board, availableSquares: Square[]) {
         for (let i = square.row + 1; i <= 7; i++) {
             candidateSquare = (Square.at(i, square.col));
-            if (this.isSpaceValid(board, candidateSquare)) {
+            if (this.isSpaceEmpty(board, candidateSquare)) {
                 availableSquares.push(candidateSquare);
+            } else if (this.isPieceTakeable(board, candidateSquare)) {
+                availableSquares.push(candidateSquare);
+                break;
             } else {
                 break;
             }
         }
         for (let i = square.row - 1; i >= 0; i--) {
             candidateSquare = (Square.at(i, square.col));
-            if (this.isSpaceValid(board, candidateSquare)) {
+            if (this.isSpaceEmpty(board, candidateSquare)) {
                 availableSquares.push(candidateSquare);
+            } else if (this.isPieceTakeable(board, candidateSquare)) {
+                availableSquares.push(candidateSquare);
+                break;
             } else {
                 break;
             }
         }
         for (let i = square.col - 1; i >= 0; i--) {
             candidateSquare = (Square.at(square.row, i));
-            if (this.isSpaceValid(board, candidateSquare)) {
+            if (this.isSpaceEmpty(board, candidateSquare)) {
                 availableSquares.push(candidateSquare);
+            } else if (this.isPieceTakeable(board, candidateSquare)) {
+                availableSquares.push(candidateSquare);
+                break;
             } else {
                 break;
             }
         }
         for (let i = square.col + 1; i <= 7; i++) {
             candidateSquare = (Square.at(square.row, i));
-            if (this.isSpaceValid(board, candidateSquare)) {
+            if (this.isSpaceEmpty(board, candidateSquare)) {
                 availableSquares.push(candidateSquare);
+            } else if (this.isPieceTakeable(board, candidateSquare)) {
+                availableSquares.push(candidateSquare);
+                break;
             } else {
                 break;
             }
@@ -61,36 +72,61 @@ export default class Piece {
     protected GetDiagonalMovement(square: Square, candidateSquare: Square | undefined, board: Board, availableSquares: Square[]) {
         for (let i = 1; Math.max(square.col + i, square.row + i) <= 7; i++) {
             candidateSquare = Square.at(square.row + i, square.col + i);
-            if (this.isSpaceValid(board, candidateSquare)) {
+            if (this.isSpaceEmpty(board, candidateSquare)) {
                 availableSquares.push(candidateSquare);
+            } else if (this.isPieceTakeable(board, candidateSquare)) {
+                availableSquares.push(candidateSquare);
+                break;
             } else {
                 break;
             }
         }
         for (let i = 1; Math.min(square.row - i, square.col - i,) >= 0; i++) {
             candidateSquare = (Square.at(square.row - i, square.col - i));
-            if (this.isSpaceValid(board, candidateSquare)) {
+            if (this.isSpaceEmpty(board, candidateSquare)) {
                 availableSquares.push(candidateSquare);
+            } else if (this.isPieceTakeable(board, candidateSquare)) {
+                availableSquares.push(candidateSquare);
+                break;
             } else {
                 break;
             }
         }
         for (let i = 1; square.row + i <= 7 && square.col - i >= 0; i++) {
             candidateSquare = (Square.at(square.row + i, square.col - i));
-            if (this.isSpaceValid(board, candidateSquare)) {
+            if (this.isSpaceEmpty(board, candidateSquare)) {
                 availableSquares.push(candidateSquare);
+            } else if (this.isPieceTakeable(board, candidateSquare)) {
+                availableSquares.push(candidateSquare);
+                break;
             } else {
                 break;
             }
         }
         for (let i = 1; square.row - i >= 0 && square.col + i <= 7; i++) {
             candidateSquare = (Square.at(square.row - i, square.col + i));
-            if (this.isSpaceValid(board, candidateSquare)) {
+            if (this.isSpaceEmpty(board, candidateSquare)) {
                 availableSquares.push(candidateSquare);
+
+            } else if (this.isPieceTakeable(board, candidateSquare)) {
+                availableSquares.push(candidateSquare);
+                break;
             } else {
                 break;
             }
         }
         return candidateSquare;
+    }
+
+    protected isSpaceEmpty(board: Board, checkSquare: Square) {
+        return board.getPiece(checkSquare) === undefined;
+    }
+
+    protected isPieceTakeable(board: Board, checkSquare: Square) {
+        let targetPiece: Piece | undefined = board.getPiece(checkSquare);
+        if (targetPiece?.player != this.player && !targetPiece?.isKing) {
+            return true;
+        }
+        return false;
     }
 }
