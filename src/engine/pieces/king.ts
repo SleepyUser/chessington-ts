26c1884f2e475f65e6fdb013e36f3fdd85aeb065
Sleepyuser <36637730+SleepyUser@users.noreply.github.com>
2023-10-19 12:06:06 +0100

    King can now move one square in each direction potentially

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,35 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableSquares: Square[] = [];
+        const square = board.findPiece(this);
+
+        if (square.row + 1 <= 7) {
+            availableSquares.push(Square.at(square.row + 1, square.col));
+
+        }
+        if (square.row - 1 >= 0) {
+            availableSquares.push(Square.at(square.row - 1, square.col));
+        }
+
+        if (square.col + 1 <= 7) {
+            availableSquares.push(Square.at(square.row, square.col + 1));
+            if (square.row + 1 <= 7) {
+                availableSquares.push(Square.at(square.row + 1, square.col + 1));
+            }
+            if (square.row - 1 >= 0) {
+                availableSquares.push(Square.at(square.row - 1, square.col + 1));
+            }
+        }
+        if (square.col - 1 >= 0) {
+            availableSquares.push(Square.at(square.row, square.col - 1));
+            if (square.row + 1 <= 7) {
+                availableSquares.push(Square.at(square.row + 1, square.col - 1));
+            }
+            if (square.row - 1 >= 0) {
+                availableSquares.push(Square.at(square.row - 1, square.col - 1));
+            }
+        }
+        return availableSquares;
     }
 }
